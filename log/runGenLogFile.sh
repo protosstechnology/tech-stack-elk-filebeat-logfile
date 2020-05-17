8937047672e5434b88f4c9d01324ed78e0983ee1
Protodd Technology <50879696+protosstechnology@users.noreply.github.com>
2020-05-17 20:33:14 +0700

    Add user

--- log/runGenLogFile.sh
+++ log/runGenLogFile.sh
@@ -1,1 +1,1 @@
-echo "2019-09-20 13:59:36 | [main] | INFO  | com.protoss.mockapp.LogPrint | testPrintInfo | - Test Log successfully." > mockapp.log
+echo "2019-09-20 13:59:36 | [main] | INFO  | com.protoss.mockapp.LogPrint | testPrintInfo | suriya_e | - Test Log successfully." > mockapp.log
